44c682b558df57eba159bee8155c6ab0124f7a65
chinmay <mail2chinmaya.jena@gmail.com>
2022-09-25 18:28:00 +0530

    added stream pause

--- controllers/upload.js
+++ controllers/upload.js
@@ -1,7 +1,6 @@
 var cuid = require("cuid");
 var Busboy = require("busboy");
 const csv = require("csv");
-const streamifier = require("streamifier");
 var fs = require("fs");
 
 const options = { auto_parse: true, columns: true, relax_column_count: true };
@@ -9,72 +8,56 @@ const options = { auto_parse: true, columns: true, relax_column_count: true };
 module.exports.uploadCSV = function (req, res, next) {
   let busboy = Busboy({ headers: req.headers });
   const parser = csv.parse(options);
-  const writeStream = fs.createWriteStream("../../myfile.json", { flags: "a" });
-  const queue = new BatchingQueue({
-    store: new MemoryStore(),
-    batchSize: 12,
-  });
-
+  //const writeStream = fs.createWriteStream("../../myfile.json", { flags: "a" });
   const db = req.app.locals.db;
   let dbase = db.db("yobulk");
-  let collectionName = cuid();
+  let d = new Date();
+  let collectionName = d.toLocaleTimeString();
   dbase.createCollection(collectionName, function (err, res) {
     if (err) throw err;
   });
+
+  let batch = [];
+
   // Listen for event when Busboy finds a file to stream.
   busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
     // We are streaming! Handle chunks
-
-    /*     file.on("data", function (data) {
-      // Here we can act on the data chunks streamed.
-
-      console.log(`File received chunk ${data.length}`);
-
-      streamifier
-        .createReadStream(data)
-        .pipe(
-          csv.parse({
-            columns: true,
-            from: 2,
-            ignoreEmpty: true,
-            relax_column_count: true,
-          })
-        )
-
-        .pipe(writeStream);
-    }); */
     let counter = 0;
     file
       .pipe(parser)
-      .on("readable", function () {
-        let record;
-        while ((record = parser.read()) !== null) {
-          if (counter === 0) {
-            writeStream.write("[");
-            writeStream.write(JSON.stringify(record) + ",");
-            counter++;
-          } else {
-            writeStream.write(JSON.stringify(record) + ",");
-          }
+      .on("data", (row) => {
+        if (batch.length === 50) {
+          parser.pause();
+          ++counter;
+          dbase
+            .collection(collectionName)
+            .insertMany(batch)
+            .then((data) => {
+              console.log("done pushing batch", counter);
+              batch.splice(0, batch.length);
+              console.log(batch.length);
+              parser.resume();
+            });
+        } else {
+          //row = { ...row, _id: cuid() };
+          batch.push(row);
         }
       })
+
       .on("end", function () {
-        writeStream.write("]");
-        /*   dbase.collection(collectionName).insertMany([
-         
-        ]); */
+        //  writeStream.write("]");
+        if (batch.length > 0) {
+          dbase
+            .collection(collectionName)
+            .insertMany(batch)
+            .then((data) => {
+              console.log("done pushing batch", counter);
+              batch.splice(0, batch.length);
+              console.log(batch.length);
+            });
+        }
       })
-      //.pipe(writeStream)
-      //.on("data", (row) => myFile.write(JSON.stringify(row)))
-      /*       .on("data", async (row) => {
-        await dbase
-          .collection(collectionName)
-          .insertOne(row, function (err, res) {
-            if (err) throw err;
-            // console.log("1 record inserted");
-          });
-      }) */
-      // .pipe(myFile)
+
       .on("error", (err) => console.log(err));
 
     // Completed streaming the file.
